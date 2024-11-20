08d7c867fabaf0eb84daa89b85598cd9c785f989
montelzek <patrykmatras378@gmail.com>
2024-11-20 21:28:50 +0100

    Creted fields for Task entity

---
+++ src/main/java/com/todo/app/entity/Task.java
@@ -0,0 +1,30 @@
+package com.todo.app.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    private Boolean completed = false;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private String updatedAt;
+
+
+}
